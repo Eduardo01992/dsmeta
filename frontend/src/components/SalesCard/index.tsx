4ee1b1568a2656cdca4b84c2bf4e9c01ef59940f
Eduardo Melo <eduardo.melo01992@gmail.com>
2022-07-16 22:59:04 -0300

    Other components

---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,90 @@
+import NotificationButton from '../NotificationButton'
+import './styles.css'
+
+function SalesCard() {
+    return (
+        <div className="dsmeta-card">
+            <h2 className="dsmeta-sales-title">Vendas</h2>
+            <div>
+                <div className="dsmeta-form-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+                <div className="dsmeta-form-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+            </div>
+
+            <div>
+                <table className="dsmeta-sales-table">
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Data</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Visitas</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">08/07/2022</td>
+                            <td>Bruna</td>
+                            <td className="show992">29</td>
+                            <td className="show992">26</td>
+                            <td>R$ 55850.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#291</td>
+                            <td className="show576">04/07/2022</td>
+                            <td>João</td>
+                            <td className="show992">40</td>
+                            <td className="show992">23</td>
+                            <td>R$ 53400.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#538</td>
+                            <td className="show576">20/07/2022</td>
+                            <td>Joana</td>
+                            <td className="show992">32</td>
+                            <td className="show992">20</td>
+                            <td>R$ 52700.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#159</td>
+                            <td className="show576">24/07/2022</td>
+                            <td>Maria</td>
+                            <td className="show992">21</td>
+                            <td className="show992">17</td>
+                            <td>R$ 49350.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default SalesCard
